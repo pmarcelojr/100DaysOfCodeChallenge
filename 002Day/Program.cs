060166ac6e3694082daa19929cb9179b56a52061
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-14 20:32:02 -0300

    criando estrutura de deposito inicial

--- 002Day/Program.cs
+++ 002Day/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace _002Day
 {
@@ -6,8 +7,26 @@ namespace _002Day
     {
         static void Main(string[] args)
         {
-            Conta p = new Conta("marcelo", 500.00, 8282);
-            Console.WriteLine(p);
+            double dep;
+
+            Console.Clear();
+            Console.Write("Entre com o número da conta: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o nome do Titular da conta: ");
+            string nome = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            string x = Console.ReadLine();
+            if(x == "s" || x == "S")
+            {
+                Console.Write("Entre com um valor de depósito inicial: ");
+                dep = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            else
+                dep = 0.0;
+            
+            Conta p = new Conta(nome, dep, num);    
+            Console.WriteLine($"\nDados da conta:\n{p}");
+
         }
     }
 }
