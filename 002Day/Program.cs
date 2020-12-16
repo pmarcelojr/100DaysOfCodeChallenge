d637e6cc665c390d7a0e7fa6f27448a151cff6ec
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-16 15:23:02 -0300

    alterando tipo de deposito inicial 002Day

--- 002Day/Program.cs
+++ 002Day/Program.cs
@@ -16,9 +16,9 @@ namespace _002Day
             Conta p = new Conta("", 0, num);
             Console.Write("Entre com o nome do Titular da conta: ");
             p.Nome = Console.ReadLine();
-            Console.Write("Haverá depósito inicial (s/n)? ");
-            string x = Console.ReadLine();
-            if(x == "s" || x == "S")
+            Console.Write("Haverá depósito inicial (S/N)? ");
+            char x = char.Parse(Console.ReadLine());
+            if(x == 'S' || x == 's')
             {
                 Console.Write("Entre com um valor de depósito inicial: ");
                 dep = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
