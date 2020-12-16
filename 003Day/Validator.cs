44a8d6e19bd2a15fd7bfe1e97e883faa1ddc2516
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-16 20:50:02 -0300

    criando method static validando email

--- 003Day/Validator.cs
+++ 003Day/Validator.cs
@@ -4,6 +4,15 @@ namespace _003Day
 {
     class Validator
     {
-
+        public static bool IsValidEmail(string email)
+        {
+            try {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch {
+                return false;
+            }
+        }
     }
 }
