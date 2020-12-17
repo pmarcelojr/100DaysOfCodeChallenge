1269cc82d43e142244b5973f2fd37c1cc5e68c7b
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-17 14:24:33 -0300

    creating list Employee add and updated list

--- 005Day/Program.cs
+++ 005Day/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
 
 namespace _005Day
 {
@@ -6,7 +8,29 @@ namespace _005Day
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("How many employees will be registered? ");
+            int x = int.Parse(Console.ReadLine());
+
+            List<Employee> list = new List<Employee>();
+
+            for(int i = 0; i < x; i++)
+            {
+                Console.Clear();
+                Console.Write($"Employee #{i + 1}:\nId: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new Employee(id, name, salary));
+            }
+
+            Console.Clear();
+            Console.WriteLine("Updated list of Employee:");
+            foreach(Employee obj in list)
+            {
+                Console.WriteLine(obj);
+            }
         }
     }
 }
