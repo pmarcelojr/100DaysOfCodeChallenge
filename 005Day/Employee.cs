6f8185f0d00c8e8e374e707b887256579e4c5ba3
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-17 14:19:06 -0300

    including  tostring in class employee

--- 005Day/Employee.cs
+++ 005Day/Employee.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 
 namespace _005Day
 {
@@ -8,9 +9,21 @@ namespace _005Day
         public string Name { get; set; }
         public double Salary { get; set; }
 
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
         public void IncreaseSalary(double porcentagem)
         {
             Salary += (porcentagem / 100);
+        }
+        public override string ToString()
+        {
+            return Id + ", " + Name + ", "
+                + Salary.ToString("F2", CultureInfo.InvariantCulture);
         }
     }
 }
