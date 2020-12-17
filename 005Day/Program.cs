beaa1e3b57b9f169b5341e5c763604f178224cf7
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-17 17:38:12 -0300

    criando estrutura logica

--- 005Day/Program.cs
+++ 005Day/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace _005Day
 {
@@ -6,7 +8,46 @@ namespace _005Day
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            Console.Write("How many employees will be registered? ");
+            int qtdRegister = int.Parse(Console.ReadLine());
+
+            List<Employee> list = new List<Employee>();
+
+            for(int i = 0; i < qtdRegister; i++)
+            {
+                Console.Clear();
+                Console.Write($"Employee #{i + 1}:\nId: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new Employee(id, name, salary));
+            }
+            Console.Clear();
+            Console.Write("Enter the employee id that will have salary increase: ");
+            int searchId = int.Parse(Console.ReadLine());
+
+            Employee emp = list.Find(x => x.Id == searchId);
+            if(emp != null)
+            {
+                Console.Clear();
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.IncreaseSalary(percentage);
+            }
+            else
+            {
+                Console.Clear();
+                Console.Write("This id does not exist!");
+            }
+
+            Console.Clear();
+            foreach(Employee obj in list)
+            {
+                Console.WriteLine(obj);
+            }
         }
     }
 }
