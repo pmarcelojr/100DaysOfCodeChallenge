36b044fb8baa8653e95b13e49977d9790765ac40
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-18 19:57:01 -0300

    Day 06 - Creating new Project

---
+++ 006Day/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _006Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
