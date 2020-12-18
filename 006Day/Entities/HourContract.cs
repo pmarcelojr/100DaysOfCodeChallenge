7d878089d599dc7b3b2f8598874a3c41a093d9f6
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-18 20:26:05 -0300

    creating HourContract class

---
+++ 006Day/Entities/HourContract.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace _006Day.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+        public HourContract()
+        {
+        }
+        public HourContract(DateTime date, double valueperhour, int hours)
+        {
+            Date = date;
+            ValuePerHour = valueperhour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
