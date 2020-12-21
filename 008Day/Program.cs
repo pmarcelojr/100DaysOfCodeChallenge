0571c35463a3a7ce6ed0c683448708faf8cb59c3
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-21 19:14:53 -0300

    updating welcome to program

--- 008Day/Program.cs
+++ 008Day/Program.cs
@@ -7,8 +7,13 @@ namespace _008Day
         static void Main(string[] args)
         {
             Console.Clear();
-            Console.WriteLine("Seja Bem vindo ao Robô *-*");
-            Console.Write("--------------------------");
+            Console.WriteLine("|   Seja Bem vindo ao Robô  *-*   |");
+            Console.WriteLine("|     Resultado da Mega Sena      |");
+            Console.WriteLine("|---------------------------------|");
+            Console.Write("Informe o número do concurso: ");
+            string numConcurso = Console.ReadLine();
+
+            Console.WriteLine(numConcurso);
 
         }
     }
