fc8d94ae30b76f564d845f8158a397442bb68262
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-22 14:10:07 -0300

    creating forms introduction inittial

--- 009Day/Program.cs
+++ 009Day/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using _009Day.Entities;
+using _009Day.Entities.Enums;
 
 namespace _009Day
 {
@@ -6,7 +9,23 @@ namespace _009Day
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            Console.WriteLine("Enter cliente Data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth Date (DD/MM/YYYY): ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+            Client c1 = new Client(name, email, date);
+
+            Console.WriteLine("Enter order Data: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Order order = new Order(DateTime.Now , status, c1);
+
+            Console.WriteLine(order);
         }
     }
 }
