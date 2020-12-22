51f3e4f63f8431ba91c00aac554b18131393ae9a
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-22 14:47:39 -0300

    finish logic program

--- 009Day/Program.cs
+++ 009Day/Program.cs
@@ -20,11 +20,32 @@ namespace _009Day
             Client c1 = new Client(name, email, date);
 
             Console.WriteLine("Enter order Data: ");
+            Console.WriteLine("Pending_Payment   Processing   Shipped   Delivered");
             Console.Write("Status: ");
             OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
 
             Order order = new Order(DateTime.Now , status, c1);
+            
+            Console.Clear();
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
 
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data: ");
+                Console.Write("Product name: ");
+                string produtoName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int qtd = int.Parse(Console.ReadLine());
+
+                Product p1 = new Product(produtoName, price);
+                OrderItem orderItem = new OrderItem(qtd, price, p1);
+
+                order.AddItems(orderItem);
+            }
+            Console.Clear();
             Console.WriteLine(order);
         }
     }
