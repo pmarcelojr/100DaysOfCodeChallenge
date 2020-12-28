ce799fd75032df3f5f0de2cc65acd60f4ac66579
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-28 17:51:29 -0300

    :arrow_up: Day 013 - Creating new Project Heranca

---
+++ 013Day/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _013Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
