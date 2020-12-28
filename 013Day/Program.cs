7a89efa57cc869871814276b8792754aed86acf7
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-28 19:13:15 -0300

    Implement Logic program in 013Day

--- 013Day/Program.cs
+++ 013Day/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using _013Day.Entities;
 
 namespace _013Day
 {
@@ -6,7 +9,46 @@ namespace _013Day
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            Console.Write("Enter the number of Employees: ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Employee> list = new List<Employee>();
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Employee #{i + 1} data:");
+                Console.Write("Outsourced (y/n)? ");
+                char typeEmployee = char.Parse(Console.ReadLine());
+                if(typeEmployee == 'y' || typeEmployee == 'Y')
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Hours: ");
+                    int hours = int.Parse(Console.ReadLine());
+                    Console.Write("Value per hour: ");
+                    double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Additional charge: ");
+                    double additionalChange = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalChange));
+                }
+                else
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Hours: ");
+                    int hours = int.Parse(Console.ReadLine());
+                    Console.Write("Value per hour: ");
+                    double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("PAYMENTS:");
+            foreach(Employee func in list)
+            {
+                Console.WriteLine($"{func.Name} - $ {func.Payment().ToString("f2", CultureInfo.InvariantCulture)}");
+            }
         }
     }
 }
