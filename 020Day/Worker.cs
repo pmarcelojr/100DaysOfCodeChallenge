6b276c399ceb4551dc73a283936eb1c47ff93cac
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-05 21:12:21 -0300

    I override method StartAsync in Worker.cs

--- 020Day/Worker.cs
+++ 020Day/Worker.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -13,6 +15,10 @@ namespace _020Day
     {
         private readonly ILogger<Worker> _logger;
         private readonly IServiceScopeFactory _serviceScopeFactory;
+        private IList<string> _folderPaths;
+        private IConfiguration _configuration;
+        private int _numberOfDaysBeforeDelete;
+        private int _runIntervallInHours;
 
         public Worker(ILogger<Worker> logger, IServiceScopeFactory serviceScopeFactory)
         {
@@ -27,6 +33,20 @@ namespace _020Day
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(1000, stoppingToken);
             }
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _configuration = _serviceScopeFactory.CreateScope().
+                             ServiceProvider.GetRequiredService<IConfiguration>();
+            _numberOfDaysBeforeDelete = int.Parse(_configuration
+                                        ["App.Configurations:NumberOfDaysBeforeDelete"]);
+            _runIntervallInHours = int.Parse(_configuration
+                                            ["App.Configurations:RunIntervallInHours"]);
+            _folderPaths = File.ReadAllLines(_configuration
+            ["App.Configurations:ConfigurationFilePath"]).Select(x => x.Trim()).ToList();
+
+            return base.StartAsync(cancellationToken);
         }
     }
 }
