4fd941ab2ac142e2124ad39fb35eb017c1c48beb
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-08 18:09:18 -0300

    Implementation logic StreamWriter file

--- 022Day/Program.cs
+++ 022Day/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using System.IO;
+using _022Day.Entities;
 
 namespace _022Day
 {
@@ -6,7 +9,37 @@ namespace _022Day
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string sourceFilePath = @"C:\temp\folder\in.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourceFilePath);
+
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+                string targetFolderPath = $@"{sourceFolderPath}\out";
+                string targetFilePath = $@"{targetFolderPath}\summary.txt";
+                
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] s = line.Split(',');
+                        string name = s[0];
+                        double price = double.Parse(s[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(s[2]);
+
+                        Product prod = new Product(name, price, quantity);
+
+                        sw.WriteLine($"{prod.Name}, {prod.Total().ToString("f2", CultureInfo.InvariantCulture)}");
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);                
+            }
         }
     }
 }
