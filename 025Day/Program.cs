2505a31a33cb57b7f1db998a40a95a9c657d7fb1
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-12 10:54:53 -0300

    created logic program

--- 025Day/Program.cs
+++ 025Day/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using _025Day.Entities;
+using _025Day.Services;
 
 namespace _025Day
 {
@@ -6,7 +9,27 @@ namespace _025Day
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter contract data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Date (dd/MM/yyyy): ");
+            DateTime date =DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            Console.Write("Contract value: ");
+            double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Enter number of installments: ");
+            int months = int.Parse(Console.ReadLine());
+
+            Contract contract = new Contract(number, date, value);
+
+            ContractService contractService = new ContractService(new PaypalService());
+            contractService.ProcessContract(contract, months);
+
+            Console.WriteLine("INSTALLMENTS:");
+            foreach(Installment item in contract.Installments)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
