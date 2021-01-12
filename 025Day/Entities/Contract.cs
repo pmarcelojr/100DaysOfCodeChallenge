07286e2bfbcb76e0c26ac306538a82c9ad2cc6d5
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-12 10:51:28 -0300

    Created Entities day 25

---
+++ 025Day/Entities/Contract.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+namespace _025Day.Entities
+{
+    class Contract
+    {
+        public int Number { get; set; }
+        public DateTime Date { get; set; }
+        public double TotalValue { get; set; }
+        public List<Installment> Installments { get; set; } = new List<Installment>();
+
+        public Contract(int number, DateTime date, double totalValue)
+        {
+            Number = number;
+            Date = date;
+            TotalValue = totalValue;
+        }
+
+        public void AddInstallments(Installment installment)
+        {
+            Installments.Add(installment);
+        }
+    }
+}
---
+++ 025Day/Entities/Installment.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Globalization;
+
+namespace _025Day.Entities
+{
+    class Installment
+    {
+        public DateTime DueDate { get; set; }
+        public double Amount { get; set; }
+
+        public Installment(DateTime dueDate, double amount)
+        {
+            DueDate = dueDate;
+            Amount = amount;
+        }
+
+        public override string ToString()
+        {
+            return $"{DueDate.ToString("dd/MM/yyyy")} - {Amount.ToString("f2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
