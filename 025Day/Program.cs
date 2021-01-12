62a21d47316b2a4069069aa2d52031d8933b4f8f
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-11 21:26:15 -0300

    :arrow_up: Created Project 025 Day

---
+++ 025Day/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _025Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
