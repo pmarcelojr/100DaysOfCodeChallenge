7e954d299e8112ac0f76e4112412fb071c9406b8
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-18 15:04:37 -0300

    Created Day29

---
+++ 029Day/conjuntoSet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace conjuntoSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
