5e7245fef7ed284426aa530bd5a88f2de57c746d
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-18 17:01:28 -0300

    Initial implementation conjuntoDictionary

--- 029Day/conjuntoDictionary/Program.cs
+++ 029Day/conjuntoDictionary/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace conjuntoDictionary
 {
@@ -6,7 +8,41 @@ namespace conjuntoDictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    Dictionary<string, int> list = new Dictionary<string, int>();
+
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(",");
+                        string name = line[0];
+                        int voto = int.Parse(line[1]);
+
+                        if (list.ContainsKey(name))
+                        {
+                            list[name] += voto;
+                        }
+                        else
+                        {
+                            list[name] = voto;
+                        }
+                    }
+
+                    foreach (KeyValuePair<string, int> item in list)
+                    {
+                        Console.WriteLine($"{item.Key}: {item.Value}");
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
